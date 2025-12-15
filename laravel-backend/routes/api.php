884d23df04c9723bd1f77e3c94d3ae1979824f3a
nowendan <gmbit3@gmail.com>
2025-12-15 21:29:53 +0700

    Update keuangan API controller and routes

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\JadwalController;
 use App\Http\Controllers\Api\MahasiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\KeuanganController;
 
 Route::apiResource('/Jadwal', JadwalController::class)->except(['index', 'destroy']);
 Route::apiResource('/mahasiswa', MahasiswaController::class)->except(['show']);
@@ -11,4 +12,16 @@ Route::apiResource('/mahasiswa', MahasiswaController::class)->except(['show']);
 // buat route khusus login & register
 Route::post('/login', [MahasiswaController::class, 'login']);     // login
 Route::post('/register', [MahasiswaController::class, 'store']);    // register
-Route::put('/presensi', [JadwalController::class, 'presensi']); 
+Route::put('/presensi', [JadwalController::class, 'presensi']);
+
+// route pembayaran
+Route::prefix('keuangan')->group(function () {
+    Route::get('jenis-pembayaran', [KeuanganController::class, 'jenisPembayaran']);
+    Route::post('jenis-pembayaran', [KeuanganController::class, 'storeJenisPembayaran']);
+
+    Route::get('tagihan', [KeuanganController::class, 'tagihan']);
+    Route::post('tagihan', [KeuanganController::class, 'storeTagihan']);
+
+    Route::get('pembayaran', [KeuanganController::class, 'pembayaran']);
+    Route::post('pembayaran', [KeuanganController::class, 'storePembayaran']);
+});
